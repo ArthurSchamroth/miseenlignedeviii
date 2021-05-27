110f3fd5bba44f51d94e0101232ffdaf7a0fd3ac
Arthur Schamroth <schamrotharthur@gmail.com>
2021-05-27 15:12:49 +0200

    mise en ligne

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,14 +33,14 @@ class SecurityController extends AbstractController
             return $this->redirectToRoute('security_login');
         }
 
-        return $this->render('./security/registration.html.twig', [
+        return $this->render('./Security/registration.html.twig', [
             'form'=>$form->createView()
         ]);
     }
     
     #[Route('/connexion', name: 'security_login')]
     public function login(){
-        return $this->render('./security/login.html.twig');
+        return $this->render('./Security/login.html.twig');
     }
 
     /**
